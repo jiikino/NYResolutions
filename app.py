0d12ca1c0f23caae721602b7a8a2d80d923ad96a
JessIkino <jessicaikino@icloud.com>
2025-01-10 10:31:07 -0500

    updated html/ added js file

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+
+
+app = Flask (__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+if __name__ in "__main__":
+    app.run(debug=True)
+
+
+    
+
